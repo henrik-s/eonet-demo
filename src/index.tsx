68f788b48ec45f6ecbbf86b4faf701dc112718bc
Henrik Sohlberg <henrik.sohlberg@gmail.com>
2020-03-29 23:06:57 +0200

    404 image resource fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import * as React from 'react';
 
 import {App} from '@/app/';
 
+// https://stackoverflow.com/questions/41144319/leaflet-marker-not-found-production-env
+import "leaflet/dist/images/marker-shadow.png";
+import "leaflet/dist/images/marker-icon-2x.png";
+
 const root = document.getElementById('root');
 if (root != null) {
     ReactDOM.render(
