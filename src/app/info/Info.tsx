89220ff928522b1ca2e5b9eef5242feef09bff6c
Henrik Sohlberg <henrik.sohlberg@gmail.com>
2020-03-29 03:52:10 +0200

    Some sort of responsiveness

--- src/app/info/Info.tsx
+++ src/app/info/Info.tsx
@@ -21,91 +21,93 @@ function defaultAttrGet(field: eonet.EventFields, event: eonet.Event): React.Rea
     return value;
 }
 
-const infoEntries: Array<{
-    key: eonet.EventFields,
-    title?: string
-    getValue?: (event: eonet.Event) => React.ReactNode,
-}> = [
-    {
-        key: eonet.EventFields.Title,
-    },
-    {
-        key: eonet.EventFields.Closed,
-        title: 'Status',
-        getValue: (event: eonet.Event) => {
-            return (
-                <div>
-                    {event.closed == null ?
-                        <div className={styles.open}>Open</div>
-                        :
-                        <div>
-                            <span className={styles.closed}>Closed</span>
-                            ({new Date(event.closed).toLocaleString()})
-                        </div>
-                    }
-                    <div>
-                        <span className={styles.faded}>First report:</span>
-                        {utils.getEventDate(event, false).toLocaleString()}
-                    </div>
-                    <div>
-                        <span className={styles.faded}>Latest report:</span>
-                        {utils.getEventDate(event).toLocaleString()}
-                    </div>
-                </div>
-            );
-        },
-    },
-    {
-        key: eonet.EventFields.Description,
-    },
-    {
-        key: eonet.EventFields.Categories,
-        getValue: (event: eonet.Event) => {
-            if (!_.isArray(event.categories)) return null;
-            const nodes: Array<React.ReactNode> = [];
-            event.categories.map((category, index) => { // eslint-disable-line array-callback-return
-                nodes.push(<div key={index}>{category.title}</div>);
-            });
-
-            return nodes;
-        },
-    },
-    {
-        key: eonet.EventFields.Sources,
-        getValue: (event: eonet.Event) => {
-            if (!_.isArray(event.sources)) return null;
-            const nodes: Array<React.ReactNode> = [];
-            event.sources.map((source, index) => { // eslint-disable-line array-callback-return
-                nodes.push(<a key={index} href={source.url} target='_blank'>{source.id}</a>);
-            });
-
-            return nodes;
-        },
-    },
-    {
-        key: eonet.EventFields.Link,
-        title: 'API Endpoint',
-        getValue: (event: eonet.Event) => {
-            const value = defaultAttrGet(eonet.EventFields.Link, event) as string;
-            if (value != null) {
-                return <a href={value} target='_blank'>{value}</a>;
-            }
-
-            return null;
-        },
-    },
-    {
-        key: eonet.EventFields.Geometries,
-        getValue: (event: eonet.Event) => {
-            if (!_.isArray(event.geometries)) return null;
-
-            return <Map.Component points={event.geometries} />;
-        },
-    },
-];
-
 const Info = (props: Props) => {
     const {selectedEvent = null} = props;
+
+
+    const infoEntries: Array<{
+        key: eonet.EventFields,
+        title?: string
+        getValue?: (event: eonet.Event) => React.ReactNode,
+    }> = [
+        {
+            key: eonet.EventFields.Title,
+        },
+        {
+            key: eonet.EventFields.Closed,
+            title: 'Status',
+            getValue: (event: eonet.Event) => {
+                return (
+                    <div>
+                        {event.closed == null ?
+                            <div className={styles.open}>Open</div>
+                            :
+                            <div>
+                                <span className={styles.closed}>Closed</span>
+                                ({new Date(event.closed).toLocaleString()})
+                            </div>
+                        }
+                        <div>
+                            <span className={styles.faded}>First report:</span>
+                            {utils.getEventDate(event, false).toLocaleString()}
+                        </div>
+                        <div>
+                            <span className={styles.faded}>Latest report:</span>
+                            {utils.getEventDate(event).toLocaleString()}
+                        </div>
+                    </div>
+                );
+            },
+        },
+        {
+            key: eonet.EventFields.Description,
+        },
+        {
+            key: eonet.EventFields.Categories,
+            getValue: (event: eonet.Event) => {
+                if (!_.isArray(event.categories)) return null;
+                const nodes: Array<React.ReactNode> = [];
+                event.categories.map((category, index) => { // eslint-disable-line array-callback-return
+                    nodes.push(<div key={index}>{category.title}</div>);
+                });
+
+                return nodes;
+            },
+        },
+        {
+            key: eonet.EventFields.Sources,
+            getValue: (event: eonet.Event) => {
+                if (!_.isArray(event.sources)) return null;
+                const nodes: Array<React.ReactNode> = [];
+                event.sources.map((source, index) => { // eslint-disable-line array-callback-return
+                    nodes.push(<a key={index} href={source.url} target='_blank'>{source.id}</a>);
+                });
+
+                return nodes;
+            },
+        },
+        {
+            key: eonet.EventFields.Link,
+            title: 'API Endpoint',
+            getValue: (event: eonet.Event) => {
+                const value = defaultAttrGet(eonet.EventFields.Link, event) as string;
+                if (value != null) {
+                    return <a href={value} target='_blank'>{value}</a>;
+                }
+
+                return null;
+            },
+        },
+        {
+            key: eonet.EventFields.Geometries,
+            title: `Geometries (${selectedEvent != null ? selectedEvent.geometries.length : 0})`,
+            getValue: (event: eonet.Event) => {
+                if (!_.isArray(event.geometries)) return null;
+
+                return <Map.Component points={event.geometries} />;
+            },
+        },
+    ];
 
     return (
         <div className={classnames(styles.container, selectedEvent != null ? styles.selected : null)}>
